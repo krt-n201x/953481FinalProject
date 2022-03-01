78ef545e71f2aef5f4a4e0dc10ffd9fc2cea44a0
teambena <teetanic27@gmail.com>
2022-03-02 00:57:06 +0700

    Add generate new csv function.
    The new csv consist of id,  title, instructions, image_name and ingredients.

--- main.py
+++ main.py
@@ -32,6 +32,15 @@ def get_and_clean_data():
     cleaned_ingredients = cleaned_ingredients.apply(
         lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
 
-    
+    # make new csv dataset #
+    cleaned_csv_data = {"id": id, "Title": cleaned_title, "Instructions": cleaned_instructions,
+                        "Image_Name": image_name, "Ingredients": cleaned_ingredients}
+    dataFrame = pd.DataFrame(data=cleaned_csv_data)
+
+    try:
+        dataFrame.to_csv("src/resource/Food Recipe.csv", encoding="utf8", index=False)
+        print("Generate new csv")
+    except:
+        print("Error")
 
 
