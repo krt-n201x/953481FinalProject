67a9e19d7e5fceee3a0bf0384d1d5432c1e3d426
Kitsada <kitsada_t@cmu.ac.th>
2022-03-02 04:34:16 +0700

    User can see food suggestion list

--- project/main.py
+++ project/main.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from .process import get_and_clean_data
+from .process import exampleoutput
 
 main = Blueprint('main', __name__)
+dataframe = get_and_clean_data()
+dataexanple = exampleoutput(dataframe)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data=dataexanple)
 
 @main.route('/profile')
 @login_required
