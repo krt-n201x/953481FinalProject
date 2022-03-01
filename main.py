b0799a0004c64716369b2a16de3875bf11023bed
teambena <teetanic27@gmail.com>
2022-03-02 00:53:59 +0700

    Clean data
    clean title, instructions and ingredients

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import string
+import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def get_and_clean_data():
+    data = pd.read_csv('src/resource/Food Ingredients and Recipe Dataset with Image Name Mapping.csv')
+
+    id = data['No']
+    title = data['Title'].astype(str)
+    ingredients = data['Cleaned_Ingredients'].astype(str)
+    instructions = data['Instructions'].astype(str)
+    image_name = data['Image_Name'].astype(str)
+
+    # clean title #
+    cleaned_title = title.apply(lambda s: s.translate(str.maketrans('', '', string.punctuation + u'\xa0')))
+    cleaned_title = cleaned_title.apply(lambda s: s.lower())
+    cleaned_title = cleaned_title.apply(
+        lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
+
+    # clean instructions #
+    cleaned_instructions = instructions.apply(
+        lambda s: s.translate(str.maketrans('', '', string.punctuation + u'\xa0')))
+    cleaned_instructions = cleaned_instructions.apply(lambda s: s.lower())
+    cleaned_instructions = cleaned_instructions.apply(
+        lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
+
+    # clean ingredients #
+    punctuation = "!\"#$%&'()*+,-.:;<=>?@[\]^_`{|}~/or"
+    cleaned_ingredients = ingredients.apply(lambda s: s.translate(str.maketrans('', '', punctuation + u'\xa0')))
+    cleaned_ingredients = cleaned_ingredients.apply(lambda s: s.lower())
+    cleaned_ingredients = cleaned_ingredients.apply(
+        lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
+
+    
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
