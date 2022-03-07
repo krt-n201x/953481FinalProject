30ec8edfbc02d69a7ba379113c23cf879ce224c8
Kitsada <kitsada_t@cmu.ac.th>
2022-03-08 02:03:55 +0700

    suggestion by similar ingredients

--- project/__init__.py
+++ project/__init__.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'thisismysecretkeydonotstealit'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db2.sqlite3'
 
     db.init_app(app)
 
